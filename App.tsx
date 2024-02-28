b51503cdb0d2ddddaaa1e7aa2da28e6ca829f0f9
noranawad <143720392+noranawad@users.noreply.github.com>
2024-02-28 17:41:54 -0500

    App File

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+
+
+return (
+     <Router> 
+        <div className="grid-container"> 
+        <link rel="stylesheet" href="style.css" />
+        <title>Emerald Tiger</title>
+
+         <body>
+             <div> 
+                <header className="header">
+                    </body>
+                   </div>
+                   </Router>
+                    );
+                    return <div>Home Page Content</div>;
+
+                  
+                    export default App;
+
